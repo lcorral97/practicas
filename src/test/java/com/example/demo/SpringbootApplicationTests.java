2b95dee611e7ec14aa164e05148ed3b1e88636f8
lcorral97 <lander.corral@entelgy-ibai.com>
2020-03-25 10:58:00 +0100

    tests

--- src/test/java/com/example/demo/SpringbootApplicationTests.java
+++ src/test/java/com/example/demo/SpringbootApplicationTests.java
@@ -15,8 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.demo.dao.EmpDeptoDAO;
 import com.example.demo.dao.WeatherDAO;
 import com.example.demo.exception.CustomException;
-import com.example.demo.modelo.Empleado;
-import com.example.demo.modelo.Weather;
 import com.example.demo.service.EmpDeptoService;
 import com.example.demo.service.EmpDeptoServiceImpl;
 import com.example.demo.service.WeatherService;
@@ -43,8 +41,62 @@ public class SpringbootApplicationTests {
 	}
 
 	@Test
-	public void getWeathers() throws CustomException {
+	public void getWeathersTest() throws CustomException {
 		when(weatherDao.getWeathers()).thenReturn(new ArrayList<>());
 		assertEquals(new ArrayList<>(), weatherService.getWeathers());
+	}
+	
+	@Test
+	public void getEmpleadosTest() throws CustomException {
+		when(empDeptoDAO.getEmpleados()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getEmpleados());
+	}
+	
+	@Test
+	public void getDeptosTest() throws CustomException {
+		when(empDeptoDAO.getDepartamento()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getDepartamento());
+	}
+	
+	@Test
+	public void getSecretariosTest() throws CustomException {
+		when(empDeptoDAO.getSecretarios()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getSecretarios());
+	}
+	
+	@Test
+	public void getNomSalTest() throws CustomException {
+		when(empDeptoDAO.getNomSal()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getNomSal());
+	}
+	
+	@Test
+	public void getEmpleadosOrdTest() throws CustomException {
+		when(empDeptoDAO.getEmpleadosOrd()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getEmpleadosOrd());
+	}
+	
+	@Test
+	public void getNombreDepartamentoTest() throws CustomException {
+		when(empDeptoDAO.getNombreDepartamento()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getNombreDepartamento());
+	}
+	
+	@Test
+	public void getNomCargoTest() throws CustomException {
+		when(empDeptoDAO.getNomCargo()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getNomCargo());
+	}
+	
+	@Test
+	public void getSalCom2000Test() throws CustomException {
+		when(empDeptoDAO.getSalCom2000()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getSalCom2000());
+	}
+	
+	@Test
+	public void getComisionesTest() throws CustomException {
+		when(empDeptoDAO.getComisiones()).thenReturn(new ArrayList<>());
+		assertEquals(new ArrayList<>(), empDeptoService.getComisiones());
 	}
 }
